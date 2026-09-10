d69bd03b005bf084671214f7bb00753e94461ebc
sush123-mita <sushmitasingh2194@gmail.com>
2026-09-10 21:52:02 +0530

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,7 +23,7 @@ const seedProblems = require('./domain/problems/seedData');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/lld_platform';
+const MONGO_URI = process.env.MONGO_URI ;
 
 // Middlewares
 app.use(cors({ origin: '*' }));
